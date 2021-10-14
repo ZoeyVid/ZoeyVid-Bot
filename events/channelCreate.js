7d019a1d61fe786bcc11bd9a5b76ce99bab47599
David__Craft <david@davidcraft.de>
2021-10-14 16:10:08 +0200

    First Commit

---
+++ events/channelCreate.js
@@ -0,0 +1,24 @@
+const { MessageEmbed, WebhookClient } = require('discord.js');
+
+module.exports = {
+	name: 'channelCreate',
+	async execute(channel, client) {
+        if(channel.guild.id !== "840285826020933662") return;
+        const webhook = new WebhookClient({ url: 'https://discord.com/api/webhooks/897161883616944128/6ohO1FYFwS12Ztoxs0U-3hWULJWFWVLVx30Ip7pjyBIXDMftGd8wFURUNcK_-Cxi2lJD' });
+        var embed = new MessageEmbed()
+            .setColor("#00ff08")
+            .setTitle("Neuer Channel")
+            .addField("Channel", "<#" + channel.id + ">", true)
+            .addField("Name", channel.name, true)
+            .addField("\u200B", "\u200B", false)
+            .addField("ID", channel.id, true)
+            .addField("Type", channel.type, true)
+            .setTimestamp()
+            .setFooter(channel.guild.name, channel.guild.iconURL());
+        webhook.send({
+            username: client.user.username,
+            avatarURL: client.user.avatarURL(),
+            embeds: [embed],
+        });
+	},
+};
---
+++ main.js
@@ -0,0 +1,19 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MESSAGES]});
+const util = require('minecraft-server-util');
+const http = require('http'); 
+const fs = require('fs');
+const config = require("./config.json");
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+client.login(config.token);
