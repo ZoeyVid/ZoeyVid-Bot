119b95d6f4e10f5491f5d887785407af23db7545
David__Craft <david@davidcraft.de>
2021-10-15 13:26:09 +0200

    Add Ban Log

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -6,7 +6,7 @@ module.exports = {
         if(channel.guild.id !== "840285826020933662") return;
         const webhook = new WebhookClient({ url: 'https://discord.com/api/webhooks/897161883616944128/6ohO1FYFwS12Ztoxs0U-3hWULJWFWVLVx30Ip7pjyBIXDMftGd8wFURUNcK_-Cxi2lJD' });
         var embed = new MessageEmbed()
-            .setColor("#ffff00")
+            .setColor("#00ff08")
             .setTitle("Neuer Channel")
             .addField("Channel", "<#" + channel.id + ">", true)
             .addField("Name", channel.name, true)
--- events/emojiCreate.js
+++ events/emojiCreate.js
@@ -6,7 +6,7 @@ module.exports = {
         if(emoji.guild.id !== "840285826020933662") return;
         const webhook = new WebhookClient({ url: 'https://discord.com/api/webhooks/898502035564036096/SvHbQ7zVMOD3atglNH6bOH2zhX-V4PLq6rVTs3G3SV_Qfnb_w7IWwxisbCz4rgq-zE2E' });
         var embed = new MessageEmbed()
-            .setColor("#ffff00")
+            .setColor("#00ff08")
             .setTitle("Neuer Emoji")
             .addField("Name", emoji.name, true)
             .addField("Aniemiert", String(emoji.animated), true)
