b65a8886c5d8e66f0a3a6c45763ae7db11ed54ea
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-04-02 18:55:20 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const {MessageEmbed, WebhookClient} = require("discord.js");
 
 module.exports = {
     name: "guildMemberAdd",
-    async execute(member, client) {
+    async execute(member, client, database) {
         var webhookURL;
         let promise = database.getDocument('webhook', 'member');
 
