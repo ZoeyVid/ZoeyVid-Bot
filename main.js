daaa41a35a713b871613e733713eeaa7ce1d15ea
David <contact@davidcraft.de>
2022-11-15 16:48:09 +0100

    Update main.js
    Signed-off-by: David <contact@davidcraft.de>

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection, Events } = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
