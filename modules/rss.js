51bf8fc850b5149a3902809b77a52e048e9d1161
David <contact@davidcraft.de>
2023-02-26 15:57:32 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -1,11 +1,12 @@
 const { parse } = require('rss-to-json');
 
-module.exports = async function () {
+module.exports = async function (client) {
     (async () => {
 
         var rss = await parse('https://www.tagesschau.de/xml/rss2/');
     
         console.log(JSON.stringify(rss, null, 3));
+        client.channels.cache.get('947182443637653555').send(rss.items[0]);
     
     })();
 };
