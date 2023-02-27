47b8c025ed97d2b33cd29ff3e6040fc2f2ee15d6
David <contact@davidcraft.de>
2023-02-27 15:44:52 +0000

    So?
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -8,7 +8,7 @@ module.exports = async function (client) {
         console.log(JSON.stringify(rss, null, 3));
 
         Object.keys(rss).forEach(element => {
-            console.log(element)
+            console.log(rss[element])
             if(String(rss[element].link).includes("eilmeldung")) {
                 console.log("!!!Eilmeldung gefunden: " + rss[element].link)
             }
