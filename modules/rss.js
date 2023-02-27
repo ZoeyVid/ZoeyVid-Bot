7f5903b8b34b2ad81ef8116867f530d9a3b15f3c
David <contact@davidcraft.de>
2023-02-27 15:48:46 +0000

    SOOOO
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -3,7 +3,7 @@ const { parse } = require('rss-to-json');
 module.exports = async function (client) {
     (async () => {
 
-        var rss = await parse('https://www.tagesschau.de/xml/rss2/');
+        var rss = JSON.parse(await parse('https://www.tagesschau.de/xml/rss2/'))
     
         console.log(JSON.stringify(rss, null, 3));
 
