163979b1b98d2b146dbef7adb9c69fd0e940746d
David <contact@davidcraft.de>
2023-02-28 15:50:15 +0000

    So aber jetzt oder?
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -5,7 +5,7 @@ module.exports = async function (client) {
 
         var rss = await parse('https://www.tagesschau.de/xml/rss2/')
 
-        JSON.parse(rss).forEach(element => {
+        Object.keys(JSON.parse(rss)).forEach(element => {
             console.log(element + "element")
             if(String(element.link).includes("eilmeldung")) {
                 console.log("!!!Eilmeldung gefunden: " + element.link)
