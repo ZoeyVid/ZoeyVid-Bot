0756ff4e52617dd7e5f1f8197b944c804bab7f7e
David <contact@davidcraft.de>
2023-02-28 15:55:57 +0000

    Uff
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -6,7 +6,7 @@ module.exports = async function (client) {
         var rss = await parse('https://www.tagesschau.de/xml/rss2/')
         console.log(rss)
         Object.keys(rss).forEach(element => {
-            console.log(element.link() + "element")
+            console.log(element + "element")
             if(String(element.link).includes("eilmeldung")) {
                 console.log("!!!Eilmeldung gefunden: " + element.link)
             }
