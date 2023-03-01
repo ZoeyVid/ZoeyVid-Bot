b8900506d0a67e67709c4d2402d4aa608e59bbdc
David <contact@davidcraft.de>
2023-03-01 15:48:59 +0000

    Jetzt aber
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -1,9 +1,9 @@
-const { parse } = require('rss-to-json');
+const { extract } = require('@extractus/feed-extractor')
 
 module.exports = async function (client) {
     (async () => {
 
-        var rss = await parse('https://www.tagesschau.de/xml/rss2/')
+        var rss = await extract('https://www.tagesschau.de/xml/rss2/')
         console.log(rss)
         Object.keys(rss).forEach(element => {
             console.log(element + "element")
