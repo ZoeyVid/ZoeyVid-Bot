dc233a1931beb9d542fee27f47148b9c19b2f391
David <contact@davidcraft.de>
2023-03-04 15:37:04 +0000

    JJaaa endlich geht es weiter
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -2,15 +2,10 @@ const { extract } = require('@extractus/feed-extractor')
 
 module.exports = async function () {
     console.log("RSS-Modul geladen")
-        console.log("Weiter gehts")
         var rss = await extract('https://www.tagesschau.de/xml/rss2/')
-        console.log("Hier?")
-        console.log(rss)
-        console.log(rss.link)
-        console.log(rss.entries)
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries?retiredLocale=de
         rss.entries.forEach(element => {
-            console.log(element)
+            console.log(element.link)
             if(String(element.link).includes("eilmeldung")) {
                 console.log("!!!Eilmeldung gefunden: " + element.link)
             }
