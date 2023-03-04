f56c21b0e83d1685d34dd07478c7047e9859226b
David <contact@davidcraft.de>
2023-03-04 15:53:25 +0000

    Ach egal
    Signed-off-by: GitHub <noreply@github.com>

--- modules/rss.js
+++ modules/rss.js
@@ -2,9 +2,6 @@ const { extract } = require('@extractus/feed-extractor')
 
 module.exports = async function (client) {
         console.log("RSS-Modul geladen")
-        client.channels.fetch('947182443637653555')
-        var tagesschauChannel = client.channels.cache.get('947182443637653555');
-        tagesschauChannel.send("RSS-Modul geladen")
         var rss = await extract('https://www.tagesschau.de/xml/rss2/')
         rss.entries.forEach(element => {
             if(String(element.link).includes("eilmeldung")) {
