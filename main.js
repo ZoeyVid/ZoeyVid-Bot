413b8dcb58d3abc3612721ac0f62e9b2ad05886c
Zoey <zoey@z0ey.de>
2023-05-14 22:52:24 +0200

    disable rss
    Signed-off-by: Zoey <zoey@z0ey.de>

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ for (const file of eventFiles) {
 }
 
 require("./modules/status")(Number(config.status_port), String(config.status_message));
-require("./modules/rss")(client);
+//require("./modules/rss")(client);
 //client.guilds.fetch()
 //client.guilds.cache.get("869634252826894336").channels.fetch()
 //console.log(client.channels.cache)
