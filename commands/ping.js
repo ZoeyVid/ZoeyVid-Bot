b9a7a2b07c9ecbac3b97680e72d757b300f5b8d5
David <david@davidcraft.de>
2023-10-12 15:43:13 +0200

    Und Ping
    Signed-off-by: David <david@davidcraft.de>

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Sendet die Zeit, die der Bot benötigt, um zu antworten.'),
-	async execute(interaction) {
+	async execute(interaction, client) {
 		await interaction.reply({
             content: "Pong! **" + client.ws.ping + "ms**",
             ephemeral: true,
