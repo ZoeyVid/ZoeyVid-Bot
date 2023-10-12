582df2e5440a8c5f8c7e996d5f5fc5998bcdeaa7
David <david@davidcraft.de>
2023-10-12 16:01:04 +0200

    Ok
    Signed-off-by: David <david@davidcraft.de>

--- command-deployment.js
+++ command-deployment.js
@@ -27,8 +27,6 @@ const rest = new REST().setToken(token);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		console.log(commands)
-		console.log(Routes.applicationCommands(clientid))
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(clientid),
