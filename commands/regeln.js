1a8c875ea3400f3b1dd12ea825b2c0c61939f520
Zoey <zoey@z0ey.de>
2023-10-13 22:40:41 +0200

    test
    Signed-off-by: Zoey <zoey@z0ey.de>

--- commands/regeln.js
+++ commands/regeln.js
@@ -3,12 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('regeln')
-		.setDescription('Die Pflicht Lektüre dieses Servers!'),
+		.setDescription('Die Pflicht Lektüreeeee dieses Servers!'),
 	async execute(interaction, client, config) {
         var regeln = await client.guilds.cache
       .get(config.guild)
-      .channels.cache.get(BigInt(config.role_channel))
-      .messages.fetch(BigInt(config.role_message));
+      .channels.cache.get(config.role_channel)
+      .messages.fetch(config.role_message);
 		await interaction.reply({
             content: String(regeln),
             ephemeral: true,
