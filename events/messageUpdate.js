afc076813c75122975a4d2bed248b29de226b38f
David <david@davidcraft.de>
2023-10-14 14:06:57 +0200

    Debug
    Signed-off-by: David <david@davidcraft.de>

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -3,6 +3,7 @@ const { checkMessageForDomains } = require('../modules/domainCheck.js');
 module.exports = {
 	name: 'messageUpdate',
 	async execute(message) {
+        console.log("Message Update")
         checkMessageForDomains(message);
 	},
 };
