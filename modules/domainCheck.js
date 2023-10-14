3a69cb7642ca41f46d883f2b860cc2d326d2e280
David <david@davidcraft.de>
2023-10-14 14:01:56 +0200

    change Domain Check
    Signed-off-by: David <david@davidcraft.de>

---
+++ modules/domainCheck.js
@@ -0,0 +1,36 @@
+const { WebhookClient } = require('discord.js');
+const dns               = require('node:dns');
+const teamServerClient  = new WebhookClient({ id: '1162036161842258041', token: 'EXZ1MzE29tI7KiPt-dTXUNkO54TPWIw7BDYHsvyf5z1JKbSgu_POjKPoYt7RdNPL8BXl' });
+
+module.exports = {
+    async checkMessageForDomains(message) {
+        var urls = message.content.toLowerCase().match(/(([^\s:/@]+\.)+[^\s:/@]+)/g);
+		if (!urls) return;
+		for (var i = 0; i < urls.length; i++) {
+			const options = {
+				family: 0,
+				hints:  dns.ADDRCONFIG | dns.V4MAPPED,
+			};
+			console.log("Check URL " + urls[i] + " from " + message.author.username);
+			if (urls[i].match(/^([a-z0-9-]+\.)+[a-z0-9-]+$/g) == null) {
+				message.delete();
+				teamServerClient.send({
+					content: message.author.username + ' hat folgende Nachricht gesendet, welche automatisch gelöscht wurde "' + message.content + '"', 
+				});
+				message.author.send('In deiner letzen Nachricht wurde eine Punycode Domain automatisch endeckt. Folgedesen wurde deine Nachricht gelöscht und du für zehn Minuten getimeoutet. Das weitere vorgehen endscheidet das Team.');
+				if (!message.member.permissions.has('ADMINISTRATOR')) message.member.timeout(10 * 60 * 1000, 'Automod - Timeout wegen Punycode - zehn Minuten');
+			} else {
+				await dns.lookup(urls[i], options, (err, address) => {
+					if (address == '0.0.0.0') {
+						message.delete();
+						teamServerClient.send({
+							content: message.author.username + ' hat folgende Nachricht gesendet, welche automatisch gelöscht wurde "' + message.content + '"', 
+						});
+						message.author.send('In deiner letzen Nachricht wurde eine im ZoeyVidNet gesperrte Domain automatisch endeckt. Folgedesen wurde deine Nachricht gelöscht und du für eine Stunde getimeoutet. Das weitere vorgehen endscheidet das Team.');
+						if (!message.member.permissions.has('ADMINISTRATOR')) message.member.timeout(60 * 60 * 1000, 'Automod - Timeout wegen gespeerte Domain - eine Stunde');
+					}
+				});
+			}
+		}
+    }
+}
