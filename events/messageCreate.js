7e97b10ccfff5c492d94a0a735474fe71a5f8559
David <david@davidcraft.de>
2023-10-14 12:57:47 +0200

    Wrong Posison
    Signed-off-by: David <david@davidcraft.de>

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,6 +11,7 @@ module.exports = {
 		
 		//Temporary
 		if (message.author.bot) return;
+    if (message.member.permissions.has('ADMINISTRATOR')) message.reply('Admin');
 		
 		var urls = message.content.toLowerCase().match(/(([^\s:/@]+\.)+[^\s:/@]+)/g);
 		if (!urls) return;
@@ -26,7 +27,6 @@ module.exports = {
 					content: message.author.username + ' hat folgende Nachricht gesendet, welche automatisch gelöscht wurde "' + message.content + '"', 
 				});
 				message.author.send('In deiner letzen Nachricht wurde eine Punycode Domain automatisch endeckt. Folgedesen wurde deine Nachricht gelöscht und du für zehn Minuten getimeoutet. Das weitere vorgehen endscheidet das Team.');
-				if (message.member.permissions.has('ADMINISTRATOR')) message.reply('Admin');
 				//message.guild.members.cache.find((member) => member.id === message.author.id).timeout(10 * 60 * 1000, 'Automod - Timeout wegen Punycode - zehn Minuten');
 			} else {
 				await dns.lookup(urls[i], options, (err, address) => {
