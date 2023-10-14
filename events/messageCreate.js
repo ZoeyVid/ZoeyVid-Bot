859b50b53e3d930a0b11fdea1ac50d0706173958
David <david@davidcraft.de>
2023-10-14 13:56:52 +0200

    Ok, not needed
    Signed-off-by: David <david@davidcraft.de>

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -24,7 +24,6 @@ module.exports = {
 				});
 				message.author.send('In deiner letzen Nachricht wurde eine Punycode Domain automatisch endeckt. Folgedesen wurde deine Nachricht gelöscht und du für zehn Minuten getimeoutet. Das weitere vorgehen endscheidet das Team.');
 				if (!message.member.permissions.has('ADMINISTRATOR')) message.member.timeout(10 * 60 * 1000, 'Automod - Timeout wegen Punycode - zehn Minuten');
-        console.log("Detect Punycode Domain " + urls[i] + " from " + message.author.username)
 			} else {
 				await dns.lookup(urls[i], options, (err, address) => {
 					if (address == '0.0.0.0') {
@@ -34,7 +33,6 @@ module.exports = {
 						});
 						message.author.send('In deiner letzen Nachricht wurde eine im ZoeyVidNet gesperrte Domain automatisch endeckt. Folgedesen wurde deine Nachricht gelöscht und du für eine Stunde getimeoutet. Das weitere vorgehen endscheidet das Team.');
 						if (!message.member.permissions.has('ADMINISTRATOR')) message.member.timeout(60 * 60 * 1000, 'Automod - Timeout wegen gespeerte Domain - eine Stunde');
-            console.log("Detect blocked Domain " + urls[i] + " from " + message.author.username)
 					}
 				});
 			}
