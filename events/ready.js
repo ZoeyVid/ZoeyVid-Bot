657f7735d6484e5173f9970551a2c045e5d4ef9a
David <david@davidcraft.de>
2023-10-14 14:22:50 +0200

    Ok, bad idea
    Signed-off-by: David <david@davidcraft.de>

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,7 @@
-const { checkMessageForDomains } = require('../modules/domainCheck.js');
-
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client, config) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.guilds.fetch(config.guild).channels.cache.forEach(channel => {
-			channel.messages.fetch().then(messages => {
-			  messages.forEach(message => cjheckMessageForDomains(message));
-			});
-		  });
 	},
 };
