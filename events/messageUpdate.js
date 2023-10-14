407d78cdda41e78d9aafb2d8c21e7b166a5991c0
David <david@davidcraft.de>
2023-10-14 14:08:49 +0200

    Ahh i think i know the problem
    Signed-off-by: David <david@davidcraft.de>

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: 'messageUpdate',
 	async execute(message) {
         console.log("Message Update")
+        console.log(message)
         checkMessageForDomains(message);
 	},
 };
